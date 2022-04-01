992a64f572d2f061957802ac39ea30b2d9404ea9
tam <vuthitam14082001@gmail.com>
2022-04-01 23:39:14 +0700

    bài tập tạo 2 xúc xắc

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -17,25 +17,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener {
+            rollDice()
+        }
 
-        // Do a dice roll when the app starts
         rollDice()
     }
 
-    /**
-     * Roll the dice and update the screen with the result.
-     */
     private fun rollDice() {
-        // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
-        val diceRoll = dice.roll()
+        val diceRoll1 = dice.roll()
+        val diceRoll2 = dice.roll()
 
-        // Find the ImageView in the layout
-        val diceImage: ImageView = findViewById(R.id.imageView2)
+        val diceImage1: ImageView = findViewById(R.id.imageView2)
+        val diceImage2: ImageView = findViewById(R.id.imageView3)
 
-        // Determine which drawable resource ID to use based on the dice roll
-        val drawableResource = when (diceRoll) {
+        val drawableResource1 = when (diceRoll1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -44,10 +41,17 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        // Update the ImageView with the correct drawable resource ID
-        diceImage.setImageResource(drawableResource)
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        // Update the content description
-        diceImage.contentDescription = diceRoll.toString()
+        diceImage1.setImageResource(drawableResource1)
+        diceImage2.setImageResource(drawableResource2)
+
     }
 }
