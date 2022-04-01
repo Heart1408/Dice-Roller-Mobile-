75152e107661075103c3312dbdbb3888e16142a9
tam <vuthitam14082001@gmail.com>
2022-04-01 23:04:46 +0700

    Hiển thị dice images chính xác dựa trên dice roll

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -23,30 +23,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultTextView: TextView = findViewById(R.id.textView)
-        val myFirstDice = Dice(6)
-        val rollResult = myFirstDice.roll()
-        val luckyNumber = 4
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
 
-        when (rollResult) {
-            luckyNumber -> {
-                resultTextView.text = "You won!"
-            }
-            1 -> {
-                resultTextView.text = "So sorry! You rolled a 1. Try again!"
-            }
-            2 -> {
-                resultTextView.text = "Sadly, you rolled a 2. Try again!"
-            }
-            3 -> {
-                resultTextView.text = "Unfortunately, you rolled a 3. Try again!"
-            }
-            5 -> {
-                resultTextView.text = "Don't cry! You rolled a 5. Try again!"
-            }
-            6 -> {
-                resultTextView.text = "Apologies! You rolled a 6. Try again!"
-            }
+        val diceImage: ImageView = findViewById(R.id.imageView2)
+
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
         }
     }
 }
